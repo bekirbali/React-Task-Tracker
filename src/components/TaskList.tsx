9bf0fc996d45061a4050f709fccfcbe57d782c84
bekirbali <bfbali43@gmail.com>
2023-03-03 23:08:18 +0300

    adding and show hide

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,7 +1,20 @@
-const TaskList = () => {
+const TaskList = ({ showTask }) => {
+  console.log(showTask);
   return (
     <div>
-      <h1>TaskList</h1>
+      <ul>
+        {showTask?.map((task, index) => {
+          return (
+            <li
+              key={index}
+              className="bg-white text-black px-2 py-1 rounded-md my-2"
+            >
+              <p className="font-bold tracking-wide">{task.task}</p>
+              <p className="font-medium">{task.date.split("T").join(" ")}</p>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
